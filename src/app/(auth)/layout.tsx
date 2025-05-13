3cc1fd3ebd943ee27c35aab7d39c81195bb127f2
Jong44 <mat.skak45@gmail.com>
2025-05-13 14:17:14 +0700

    setup project

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { AppSidebar } from '@/components/app-sidebar'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { Separator } from '@radix-ui/react-separator'
+import React from 'react'
+
+const AuthLayout = ({children} : any) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
+
+export default AuthLayout
